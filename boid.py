56e467bfbec61037936c66e24b933e7c12f4791c
AdrienSF <adriensf@gmail.com>
2020-06-02 14:40:31 +0200

    clean up code

--- boid.py
+++ boid.py
@@ -1,26 +1,22 @@
 import numpy as np
 
-# idea: imitate relative position and velocity:
-
-
 class Boid():
     def __init__(self):
         self.view_range = 50
         self.collision_range = 20
-        self.wall_collision_range = 0
         self.speed_cap = 50
+
         # set 0 velocity
         self.velocity = np.array([0, 0])
-        # and position
-        self.position = np.array([np.random.uniform(0, 640 - self.wall_collision_range), np.random.uniform(0, 480 - self.wall_collision_range)])
-        # and acceleration
+        # set random position
+        self.position = np.array([np.random.uniform(0, 640), np.random.uniform(0, 480)])
+        # and random acceleration
         self.acceleration = np.array([np.random.uniform(-10, 10), np.random.uniform(-10, 10)])
 
         self.match_position_factor = 1
         self.match_velocity_factor = 0
         self.match_acceleration_factor = 1
         self.collision_factor = 20
-        self.wall_collision_boost = 10
 
 
     def move(self, dt):
@@ -29,35 +25,14 @@ class Boid():
         self.position = np.array(self.position) + np.array(self.velocity) * dt
 
 
-    def getRotation(self):
-        unit_velocity = np.array(self.velocity) / np.linalg.norm(self.velocity)
-        angle = np.arccos(unit_velocity[0]) * np.sign(unit_velocity[1])
-        # convert from radians to negative degrees (this is what pyglet uses, I don't make the rules)
-        to_return = -1 * angle * (180/np.pi)
-        return to_return
-
-    def set_new_velocity(self, flock: list):
-        # get all other boids within view range
-        in_view = [ boid for boid in flock if np.linalg.norm(np.array(boid.position) - np.array(self.position)) < self.view_range and boid != self ]
-        
-        new_velocity = np.array(self.velocity) + self.avoid_collision(in_view)
-        if in_view:
-            new_velocity = new_velocity + self.match_position(in_view) + self.match_velocity(in_view)
-
-        self.velocity = new_velocity
-        # make sure boid is within speed limit:
-        if np.linalg.norm(self.velocity) > self.speed_cap:
-            self.velocity = self.speed_cap * self.velocity / np.linalg.norm(self.velocity)
-            self.acceleration = np.array([0, 0])
-
     def set_new_acceleration(self, flock: list):
         in_view = [ boid for boid in flock if np.linalg.norm(np.array(boid.position) - np.array(self.position)) < self.view_range and boid != self ]
 
-        new_accelerattion = np.array(self.acceleration) + self.avoid_collision(in_view)
+        # calculat new acceleration only if other boids are in view
         if in_view:
-            new_accelerattion = new_accelerattion + self.match_position(in_view) + self.match_acceleration(in_view)
-        
-        self.acceleration = new_accelerattion
+            new_accelerattion = np.array(self.acceleration) + self.avoid_collision(in_view) + self.match_position(in_view) + self.match_acceleration(in_view)        
+            self.acceleration = new_accelerattion
+
         # make sure boid is within speed limit, if exeeded, reset acceleration to random
         if np.linalg.norm(self.velocity) > self.speed_cap:
             self.velocity = self.speed_cap * self.velocity / np.linalg.norm(self.velocity)
@@ -66,30 +41,17 @@ class Boid():
 
     def avoid_collision(self, flock: list):
         # make screen wrap around
-        boost = 1
         if self.position[0] < 0:
-            # self.acceleration[0] = 0
             self.position[0] = 640
-            # boost = self.wall_collision_boost
 
         if self.position[1] < 0:
-            # self.acceleration[1] = 0
             self.position[1] = 480
-            # boost = self.wall_collision_boost
 
         if self.position[0] > 640:
-            # self.acceleration[0] = 0
             self.position[0] = 0
-            # boost = self.wall_collision_boost
 
         if self.position[1] > 480:
-            # self.acceleration[1] = 0
             self.position[1] = 0
-            # boost = self.wall_collision_boost
-
-        # if should_return:
-            # return np.array([0, 0])
-
 
         # avoid other boids
         other_boids = [ np.array(other.position) - np.array(self.position) for other in flock if np.linalg.norm(np.array(self.position) - np.array(other.position)) < self.collision_range]
@@ -97,23 +59,11 @@ class Boid():
             avoid_obs = -1 * np.mean(other_boids, axis=0)
         else:
             avoid_obs = np.array([0.0, 0.0])
-        # # avoid edges of screen
-        # if self.position[0] < 0 + self.wall_collision_range:
-        #     avoid_obs -= np.array([-1*self.position[0], 0])
-
-        # if self.position[1] < 0 + self.wall_collision_range:
-        #     avoid_obs -= np.array([0, -1*self.position[1]])
-
-        # if self.position[0] > 640 - self.wall_collision_range:
-        #     avoid_obs -= np.array([640 - self.position[0], 0])
-
-        # if self.position[1] > 480 - self.wall_collision_range:
-        #     avoid_obs -= np.array([0, 480 - self.position[1]])
         
         # normalize avoidance vector, otherwise there's less "avoidance" the closer boids are to colliding 
         if np.linalg.norm(avoid_obs) != 0:
             avoid_obs = avoid_obs / np.linalg.norm(avoid_obs)
-        return self.collision_factor * avoid_obs #* boost
+        return self.collision_factor * avoid_obs
 
     def match_position(self, flock: list):
         towards_center = np.mean([ boid.position for boid in flock ],  axis=0) - np.array(self.position)
--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from boid import Boid
 window = pyglet.window.Window()
 
 
-# arrow_image = pyglet.image.load('arrow.png')
 # create boids
 boids = []
 for i in range(50):
@@ -18,18 +17,13 @@ for point_boid in boids:
                 ('v2f', tuple(point_boid.position))
             ))
 
-# print([ list(pyglet_obj.vertices) for pyglet_obj in vertex_list])
 
 def move_boids(dt):
     for i in range(len(boids)):
-        # use a 100 x smaller timestep to ensure colisions are handled properly. 
-        # This shouldn't be computationally very difficult.. except it is!?
-        # for t in range(100):
         boids[i].move(dt)
         boids[i].set_new_acceleration(boids)
         # set points to be at the same position as the boid objects
         vertex_list[i].vertices = list(boids[i].position)
-    # print('moved all boids')
 
 
 
@@ -40,6 +34,6 @@ def on_draw():
     batch.draw()
 
 
-
+# call move_boids 120 times a second
 pyglet.clock.schedule_interval(move_boids, 1/120.0)
 pyglet.app.run()
