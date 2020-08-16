885a60ee9981e7c970cd1de843f55959bf15cbee
AdrienSF <adriensf@gmail.com>
2020-08-16 13:40:34 +0200

    add movement function imitation

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
-## Accelerate branch
-In this branch, each boid imitates the acceleration vector of the boids around it, instead of the velocity vector. This implementation actually results in a flocking behavior more typical of the boids algorithm than the basic master branch implementation. In the current weight configuration, it is easy to observe sudden changes in direction and velocity that resemble flocking behavior of small and agile animals. If the match_acceleration weight is set to be negligibly small, the boids exhibit an "orbiting" behavior around the flock's center of mass. The moon orbiting the earth is constantly accelerating towards the earth, similarly the boids are continuously accelerating towards the center of mass of the flock. Furthermore, the boids try to avoid collision, which keeps them from all collapsing into a single point.
- 
-<!--
--noticed distinct fractale-like empty circles when frame by frame with 100 boids
--->
+## ?? branch
+In the boids algorithm, each boid's velocity is a function of their current velocity, the mean velocity of the boids around it, and the mean position of the boids around it. The boids algorithm uses a specific function to produce flocking behaviour, but what if each boid starts with a random function? In this branch, each boid copies this function from the boids around it, adding it to it's own. What will happen...?
+
+I'm sticking with linear functions for now:
+
+velocity = A*self.velocity + B*flock.velocity + C*flock.relative_position
+
+new_func = old_func + weight * flock_func
--- boid.py
+++ boid.py
@@ -2,44 +2,58 @@ import numpy as np
 
 class Boid():
     def __init__(self):
-        self.view_range = 50
-        self.collision_range = 20
+        self.view_range = 100
+        # self.collision_range = 20
         self.speed_cap = 50
 
-        # set 0 velocity
-        self.velocity = np.array([0, 0])
+        # set random velocity
+        self.velocity = np.array([np.random.uniform(-10, 10), np.random.uniform(-10, 10)])
         # set random position
         self.position = np.array([np.random.uniform(0, 640), np.random.uniform(0, 480)])
-        # and random acceleration
-        self.acceleration = np.array([np.random.uniform(-10, 10), np.random.uniform(-10, 10)])
 
-        self.match_position_factor = 1
-        self.match_velocity_factor = 0
-        self.match_acceleration_factor = 1
-        self.collision_factor = 20
+        self.immitation_factors = {'A': 1, 'B': 1, 'C': 1}
+        # set random movement function
+        A = np.array([np.random.uniform(-1, 1), np.random.uniform(-1, 1)])
+        B = np.array([np.random.uniform(-1, 1), np.random.uniform(-1, 1)])
+        C = np.array([np.random.uniform(-1, 1), np.random.uniform(-1, 1)])
+        self.movement_func_coefs = {'A': A, 'B': B, 'C': C}
 
 
     def move(self, dt):
-        # acceleration adds to velocity, velocity adds to position
-        self.velocity = np.array(self.velocity) + np.array(self.acceleration) * dt
         self.position = np.array(self.position) + np.array(self.velocity) * dt
+        self.screenwrap()
 
 
-    def set_new_acceleration(self, flock: list):
+    def set_new_velocity(self, flock: list, dt):
+        flock_velocity = np.array([])
+        flock_relative_pos = np.array([])
         in_view = [ boid for boid in flock if np.linalg.norm(np.array(boid.position) - np.array(self.position)) < self.view_range and boid != self ]
 
-        # calculat new acceleration only if other boids are in view
         if in_view:
-            new_accelerattion = np.array(self.acceleration) + self.avoid_collision(in_view) + self.match_position(in_view) + self.match_acceleration(in_view)        
-            self.acceleration = new_accelerattion
+            self.set_new_coefs(in_view, dt)
+            #inefficiency here may be an issue:
+            flock_velocity = np.mean([ boid.velocity for boid in in_view ], axis=0)
+            flock_relative_pos = np.mean([ boid.position for boid in in_view ], axis=0) - self.position
 
-        # make sure boid is within speed limit, if exeeded, reset acceleration to random
+        self.velocity = self.velocity + self.movement_func(flock_velocity, flock_relative_pos) * dt
+
+        # speed cap
         if np.linalg.norm(self.velocity) > self.speed_cap:
-            self.velocity = self.speed_cap * self.velocity / np.linalg.norm(self.velocity)
-            self.acceleration = np.array([np.random.uniform(-10, 10), np.random.uniform(-10, 10)])
+            self.velocity = self.speed_cap * (self.velocity / np.linalg.norm(self.velocity))
 
+    def movement_func(self, flock_velocity, flock_relative_pos):
+        # if there are no boids in view, return only the constant part of the movement function
+        if flock_relative_pos.size == 0 and flock_velocity.size == 0:
+            return self.movement_func_coefs['C']
+        else:
+            return self.movement_func_coefs['A'] * flock_velocity + self.movement_func_coefs['B'] * flock_relative_pos + self.movement_func_coefs['C']
 
-    def avoid_collision(self, flock: list):
+    def set_new_coefs(self, boids, dt): #inefficiency here may be an issue
+        self.movement_func_coefs['A'] += self.immitation_factors['A'] * np.mean([ boid.movement_func_coefs['A'] for boid in boids ], axis=0) * dt
+        self.movement_func_coefs['B'] += self.immitation_factors['B'] * np.mean([ boid.movement_func_coefs['B'] for boid in boids ], axis=0) * dt
+        self.movement_func_coefs['C'] += self.immitation_factors['C'] * np.mean([ boid.movement_func_coefs['C'] for boid in boids ], axis=0) * dt
+
+    def screenwrap(self):
         # make screen wrap around
         if self.position[0] < 0:
             self.position[0] = 640
@@ -53,26 +67,15 @@ class Boid():
         if self.position[1] > 480:
             self.position[1] = 0
 
-        # avoid other boids
-        other_boids = [ np.array(other.position) - np.array(self.position) for other in flock if np.linalg.norm(np.array(self.position) - np.array(other.position)) < self.collision_range]
-        if other_boids:
-            avoid_obs = -1 * np.mean(other_boids, axis=0)
-        else:
-            avoid_obs = np.array([0.0, 0.0])
+    #     # avoid other boids
+    #     other_boids = [ np.array(other.position) - np.array(self.position) for other in flock if np.linalg.norm(np.array(self.position) - np.array(other.position)) < self.collision_range]
+    #     if other_boids:
+    #         avoid_obs = -1 * np.mean(other_boids, axis=0)
+    #     else:
+    #         avoid_obs = np.array([0.0, 0.0])
         
-        # normalize avoidance vector, otherwise there's less "avoidance" the closer boids are to colliding 
-        if np.linalg.norm(avoid_obs) != 0:
-            avoid_obs = avoid_obs / np.linalg.norm(avoid_obs)
-        return self.collision_factor * avoid_obs
+    #     # normalize avoidance vector, otherwise there's less "avoidance" the closer boids are to colliding 
+    #     if np.linalg.norm(avoid_obs) != 0:
+    #         avoid_obs = avoid_obs / np.linalg.norm(avoid_obs)
+    #     return self.collision_factor * avoid_obs
 
-    def match_position(self, flock: list):
-        towards_center = np.mean([ boid.position for boid in flock ],  axis=0) - np.array(self.position)
-        return self.match_position_factor * towards_center
-
-    def match_velocity(self, flock: list):
-        towards_direction = np.mean([ boid.velocity for boid in flock ], axis=0) - np.array(self.velocity)
-        return self.match_velocity_factor * towards_direction
-
-    def match_acceleration(self, flock: list):
-        towards_direction = np.mean([ boid.acceleration for boid in flock ], axis=0) - np.array(self.acceleration)
-        return self.match_acceleration_factor * towards_direction
